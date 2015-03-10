12d227de89801fb910ba53931e19f73f4c2f2d79
kvstark <kvstark@users.noreply.github.com>
2015-03-09 18:02:17 -0700

    Update shelves.rb
    Using just a plain request might have worked in the past but now it causes an error (403).  Using the oauth_request instead fixes the problem.

--- lib/goodreads/client/shelves.rb
+++ lib/goodreads/client/shelves.rb
@@ -4,7 +4,7 @@ module Goodreads
     # Get books from a user's shelf
     def shelf(user_id, shelf_name, options={})
       options = options.merge(:shelf => shelf_name, :v =>2)
-      data = request("/review/list/#{user_id}.xml", options)
+      data = oauth_request("/review/list/#{user_id}.xml", options)
       reviews = data['reviews']['review']
 
       books = []
